4aef74be095d29e435ba8d763bfa337e45c350b8
EVogel1999 <eav990@gmail.com>
2019-02-20 00:26:25 -0500

    Create post list component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsListsComponent } from './posts-lists/posts-lists.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsListsComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-app/src/app/posts-lists/posts-lists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-posts-lists',
+  templateUrl: './posts-lists.component.html',
+  styleUrls: ['./posts-lists.component.scss']
+})
+export class PostsListsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
